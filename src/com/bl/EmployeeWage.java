ba1921b08497a4e126f00cc0276deba4a0eb61dc
sudha123 <sudhanaukala1997@gmail.com>
2022-11-10 20:38:12 +0530

    Added:partTime

--- src/com/bl/EmployeeWage.java
+++ src/com/bl/EmployeeWage.java
@@ -2,17 +2,23 @@ package com.bl;
 
 public class EmployeeWage {
     static final int IS_PRESENT = 1;
+    static final int IS_PART_TIME = 2;
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 16;
+    static final int PART_TIME_HOUR = 8;
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to EmployeeWage Program");
-        int attendance = (int) (Math.floor(Math.random() * 10)) % 2;
+        int attendance = (int) (Math.floor(Math.random() * 10)) % 3;
         //System.out.println(attendance);
         if (attendance == IS_PRESENT) {
-            System.out.println("Employee is present");
+            System.out.println("Employee is working Full Time");
             int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("Employee daily wage is: " + dailyWage);
+        } else if (attendance == IS_PART_TIME) {
+            System.out.println("Employee is working Part Time");
+            int dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
             System.out.println("Employee daily wage is: " + dailyWage);
         } else {
             System.out.println("Employee is absent");
