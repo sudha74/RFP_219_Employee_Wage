3ab9a9395d11d2886290d59fc1198e652d983484
sudha123 <sudhanaukala1997@gmail.com>
2022-11-10 20:55:41 +0530

    Added:UsingClassMethods

--- src/com/bl/EmployeeWage.java
+++ src/com/bl/EmployeeWage.java
@@ -4,40 +4,44 @@ public class EmployeeWage {
     static final int IS_FULL_TIME = 1;
     static final int IS_PART_TIME = 2;
     static final int WAGE_PER_HOUR = 20;
-    static final int FULL_DAY_HOUR = 8;
-    static final int PART_TIME_HOUR = 4;
     static final int WORKING_DAY_PER_MONTH = 20;
     static final int TOTAL_WORK_HOURS = 100;
-    static int totalHours = 0;
-    static int day = 0;
-    static int totalWage = 0;
-    static int dailyWage = 0;
 
-    public static void main(String[] args) {
 
-        System.out.println("Welcome to EmployeeWage Program");
+    static void calculateWage() {
+
+        int empHrs;
+        int totalHours = 0;
+        int dailyWage;
+        int day = 0;
+        int totalWage = 0;
 
         while ((totalHours < TOTAL_WORK_HOURS) && (day < WORKING_DAY_PER_MONTH)) {
 
             int attendance = (int) (Math.floor(Math.random() * 10)) % 3;
             switch (attendance) {
                 case IS_FULL_TIME:
-                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                    totalHours += FULL_DAY_HOUR;
+                    empHrs = 8;
                     break;
                 case IS_PART_TIME:
-
-                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-                    totalHours += PART_TIME_HOUR;
+                    empHrs = 4;
                     break;
                 default:
+                    empHrs = 0;
                     break;
             }
+            totalHours += empHrs;
+            dailyWage = WAGE_PER_HOUR * empHrs;
             totalWage += dailyWage;
             day++;
+            System.out.println("Day " + day + " Emp work hour " + empHrs + " wage is " + dailyWage);
         }
         System.out.println("Total work hours is : " + totalHours);
         System.out.println("Total work days are: " + day);
         System.out.println("Total wage is: " + totalWage);
+    }
+
+    public static void main(String[] args) {
+        calculateWage();
     }
 }
