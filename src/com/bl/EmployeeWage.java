9072b219165d844e0798b5f2cca879210a6eee6a
sudha123 <sudhanaukala1997@gmail.com>
2022-11-14 18:56:10 +0530

    Added:CompanyEmployeeWage

--- src/com/bl/EmployeeWage.java
+++ src/com/bl/EmployeeWage.java
@@ -2,11 +2,9 @@ package com.bl;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        EmpWageBuilder bel = new EmpWageBuilder("BEL", 10, 20, 100);
-        EmpWageBuilder situsAMC = new EmpWageBuilder("SitusAMC", 20, 22, 90);
-        bel.calculateWage();
-        System.out.println(bel);
-        situsAMC.calculateWage();
-        System.out.println(situsAMC);
+        EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+        empWageBuilder.addCompanyEmpWage("BEL", 10, 20, 100);
+        empWageBuilder.addCompanyEmpWage("situsAMC", 20, 22, 90);
+        empWageBuilder.calculateWage();
     }
 }
